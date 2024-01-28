bdb01d0f6d54840cfec27267528afd6f38ea3a2a
Dmitrii Ladnov <ladnov.dmitry@yandex.ru>
2024-01-28 19:20:21 +0300

    Update Util.java(connection to the database has been implemented)

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,10 +1,13 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.Connection;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
+    Connection connection = Util.getConnection();
     public UserDaoJDBCImpl() {
 
     }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,20 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    // реализуйте настройку соединения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/mysql_schema";
+    private static final String USER_NAME = "dmitrii";
+    private static final String PASSWORD = "password";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER_NAME, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
