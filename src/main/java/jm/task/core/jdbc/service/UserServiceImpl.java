3a0a2c6324a4147f3fbc58f6e44bea6b3c44cb1c
Dmitrii Ladnov <ladnov.dmitry@yandex.ru>
2024-01-28 21:21:08 +0300

    Maven clean Update application.properties Update UserServiceImpl.java (added class reuse UserDaoJDBCImpl.java)

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,31 +1,34 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    public void createUsersTable() {
+    private final UserDaoJDBCImpl userDaoJDBCImpl = new UserDaoJDBCImpl();
 
+    public void createUsersTable() {
+        userDaoJDBCImpl.createUsersTable();
     }
 
     public void dropUsersTable() {
-
+        userDaoJDBCImpl.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        userDaoJDBCImpl.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
-
+        userDaoJDBCImpl.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        return null;
+        return userDaoJDBCImpl.getAllUsers();
     }
 
     public void cleanUsersTable() {
-
+        userDaoJDBCImpl.cleanUsersTable();
     }
 }
