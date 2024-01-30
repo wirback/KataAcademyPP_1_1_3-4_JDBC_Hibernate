b20ca1f176c914eb928b1206c2b0fd6d0f7b07de
Dmitrii Ladnov <ladnov.dmitry@yandex.ru>
2024-01-31 00:15:34 +0300

    Update Main.java

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -10,16 +10,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
-        /**
-         * ? В методе main класса Main должны происходить следующие операции:
-         * * 1. Создание таблицы User(ов)
-         * * 2. Добавление 4 User(ов) в таблицу с данными на свой выбор.
-         * *    После каждого добавления должен быть вывод в консоль (User с именем – name добавлен в базу данных)
-         * * 3. Получение всех User из базы и вывод в консоль (должен быть переопределен toString в классе User)
-         * * 4. Очистка таблицы User(ов)
-         * * 5. Удаление таблицы
-         */
-
+        
         List<User> userList = new ArrayList<>();
         userList.add(new User("Ivan", "Ivanov", (byte) 30));
         userList.add(new User("Petr", "Petrov", (byte) 23));
